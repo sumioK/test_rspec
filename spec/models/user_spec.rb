90a09239e474c327aef7b4ef9945e73d658dc254
Sumio K <tap.miwo@gmail.com>
2022-08-06 16:44:42 +0900

    fix test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+RSpec.describe User, type: :model do
+
   it "性、名を登録すると、姓名が取得できること" do
     user = User.new(
       last_name: "侍",
@@ -7,3 +9,5 @@ require 'rails_helper'
     )
     expect(user.full_name).to eq "侍　太郎"
   end
+
+end
